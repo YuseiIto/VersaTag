72b42a834b9398f451c232a329a709c65e4be2e2
Yusei Ito <me@yuseiito.com>
2025-02-17 01:26:34 +0900

    fix: Change pin assignment from Arduino UNO to ESP32

--- arduino/pin_config.h
+++ arduino/pin_config.h
@@ -4,11 +4,11 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-#define RST_PIN         8
-#define DC_PIN          9
-#define CS_PIN          10
-#define BUSY_PIN        7
-#define PWR_PIN         6
+#define RST_PIN 22
+#define DC_PIN 25
+#define CS_PIN 19
+#define BUSY_PIN 0
+#define PWR_PIN 32 // Not connected, but required by epdif.h
 
 #define BMP280_CS_PIN 21
 
