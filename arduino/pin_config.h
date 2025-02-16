e40a4b6db46bb84a7c15d858fc4e25d0288f1d31
Yusei Ito <me@yuseiito.com>
2025-02-17 01:13:18 +0900

    fix(pin_config.h): add include guard

--- arduino/pin_config.h
+++ arduino/pin_config.h
@@ -1,9 +1,13 @@
 /**
  * pin configuration
  */
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
 
 #define RST_PIN         8
 #define DC_PIN          9
 #define CS_PIN          10
 #define BUSY_PIN        7
 #define PWR_PIN         6
+
+#endif
