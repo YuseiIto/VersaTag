b33d423ecf26475e4e3a79831aa510deb1a9c0ae
Yusei Ito <me@yuseiito.com>
2025-02-17 15:41:00 +0900

    fix: fix pin assignment(Temporary)

--- arduino/pin_config.h
+++ arduino/pin_config.h
@@ -4,12 +4,13 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-#define EPD_RST_PIN 22
-#define EPD_DC_PIN 25
-#define EPD_CS_PIN 19
 #define EPD_BUSY_PIN 0
-#define EPD_PWR_PIN 32 // Not connected, but required by epdif.h
+#define EPD_RST_PIN 22
+#define EPD_DC_PIN 21
 
-#define BMP280_CS_PIN 21
+#define EPD_CS_PIN 19
+#define EPD_PWR_PIN 13
+
+#define BMP280_CS_PIN 25
 
 #endif
