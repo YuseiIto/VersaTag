fb4c259391741b6485f546b1cc74344ca6e57cf6
Yusei Ito <me@yuseiito.com>
2025-02-18 23:04:42 +0900

    fix: update pin assignment

--- arduino/pin_config.h
+++ arduino/pin_config.h
@@ -4,13 +4,13 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-#define EPD_BUSY_PIN 0
+#define EPD_BUSY_PIN 32
 #define EPD_RST_PIN 22
-#define EPD_DC_PIN 21
+#define EPD_DC_PIN 25
 
 #define EPD_CS_PIN 19
 #define EPD_PWR_PIN 13
 
-#define BMP280_CS_PIN 25
+#define BMP280_CS_PIN 21
 
 #endif
